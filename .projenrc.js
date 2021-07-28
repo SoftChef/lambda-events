e9802ad9b19bbccde3d52c1e789b6f3edace339c
MinChe Tsai <tirme0812@gmail.com>
2021-07-28 12:23:10 +0800

    Enable mergify

--- .projenrc.js
+++ .projenrc.js
@@ -1,15 +1,16 @@
-const { TypeScriptAppProject, NpmAccess, ProjectType } = require('projen');
+const { DependenciesUpgradeMechanism, TypeScriptAppProject, NpmAccess, ProjectType } = require('projen');
+
+const AUTOMATION_TOKEN = 'PROJEN_GITHUB_TOKEN';
 
 const project = new TypeScriptAppProject({
-  author: 'softchef-iot-lab',
+  author: 'SoftChef',
   authorName: 'MinChe Tsai',
   authorEmail: 'poke@softchef.com',
   npmAccess: NpmAccess.PUBLIC,
   projectType: ProjectType.LIB,
-  projenVersion: '0.24.11',
+  projenVersion: '0.27.0',
   majorVersion: 1,
   defaultReleaseBranch: 'main',
-  jsiiFqn: 'projen.TypeScriptAppProject',
   name: '@softchef/lambda-events',
   repositoryUrl: 'https://github.com/SoftChef/lambda-events.git',
   release: true,
@@ -20,12 +21,23 @@ const project = new TypeScriptAppProject({
     '@aws-sdk/client-cognito-identity-provider',
     '@types/node@15.12.2',
     '@types/semver',
+    '@types/sinon',
+    'axios',
     'joi',
     'semver',
     'sinon',
-    '@types/sinon',
-    'axios',
   ],
+  depsUpgrade: DependenciesUpgradeMechanism.githubWorkflow({
+    ignoreProjen: false,
+    workflowOptions: {
+      labels: ['auto-approve', 'auto-merge'],
+      secret: AUTOMATION_TOKEN,
+    },
+  }),
+  autoApproveOptions: {
+    secret: 'GITHUB_TOKEN',
+    allowedUsernames: ['MinCheTsai'],
+  },
   tsconfig: {
     compilerOptions: {
       moduleResolution: 'node',
